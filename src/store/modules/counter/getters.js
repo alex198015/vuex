458b8e2700fd71f1b1c29f5256a549540cd77d6a
Alex <kitten20811@gmail.com>
2022-11-23 00:04:15 +0200

    vuex

---
+++ src/store/modules/counter/getters.js
@@ -0,0 +1,22 @@
+export default {
+    // testAuth(state, getters, rootState, rootGetters) {
+    //     console.log(state, getters, rootState, rootGetters);
+    //     return rootState.isLoggedIn
+    // },
+    testAuth(state) {
+        return state.isLoggedIn
+    },
+    finalCounter(state) {
+        return state.counter * 3
+    },
+    normalizedCounter(_, getters) {
+        const finalCounter = getters.finalCounter
+        if (finalCounter < 0) {
+            return 0
+        }
+        if (finalCounter > 100) {
+            return 100
+        }
+        return finalCounter
+    }
+}
